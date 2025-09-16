dc893c4399944009f1654a23eed6fab87291b890
resoner0796 <ulisees.luna96@gmail.com>
2025-09-16 03:13:00 -0500

    Create sw-admin.js

---
+++ sw-admin.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'admin-panel-cache-v1';
+// Rutas relativas para asegurar que funcione correctamente
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon-192x192.png',
+  './icon-512x512.png',
+  './maskable-icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache del panel de admin abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
